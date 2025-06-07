ed6a78def3331b8e86b436bb562a82cfa0d95d65
JakirHossain <jakir.devbd@gmail.com>
2025-06-07 23:49:15 +0600

    Mongodb connection has established

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,11 +9,35 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGODB_APP_NAME}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let foodCollection;
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db(process.env.MONGODB_DB);
+    foodCollection = db.collection("foodCollection");
+
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+run();
+
+
+app.get("/", (req, res) => res.send("FoodCircle Backend Running"));
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Server listening on port ${port}`);
+});
