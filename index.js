34c716980e67570c9407f00b8985774c227c5208
JakirHossain <jakir.devbd@gmail.com>
2025-06-07 18:16:22 +0600

    Basic server seting done

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +17,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
