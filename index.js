6fd360982cfa0b861eee78d280c49a14b0e3c37c
JakirHossain <jakir.devbd@gmail.com>
2025-06-13 10:47:22 +0600

    Some modifications have made to index.js file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
